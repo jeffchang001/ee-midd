48db210ceb992589c250e53a6e36964fe2a32c6f
Jeff Chang <jeffchang001@gmail.com>
2024-08-31 18:00:11 +0800

    make project easyier to read

--- src/test/java/com/sogo/ee/midd/MiddApplicationTests.java
+++ src/test/java/com/sogo/ee/midd/MiddApplicationTests.java
@@ -3,7 +3,7 @@ package com.sogo.ee.midd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = MiddApplication.class)
+@SpringBootTest(classes = EeMiddApplication.class)
 class MiddApplicationTests {
 
 	@Test
